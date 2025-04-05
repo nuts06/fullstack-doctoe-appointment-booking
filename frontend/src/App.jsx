696090b23d125755a7dad99a6c976a59de482b8f
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-05 18:42:19 +0530

    added the path using the react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home'
+import Login from './pages/Login';
+import Doctors from './pages/Doctors';
 
 const App = () => {
   return (
-    <div className='underline'>App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path='/doctors' element={<Doctors />}/>
+        <Route path='/doctors/:speciality' element={<Doctors />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </div>
   )
 }
 

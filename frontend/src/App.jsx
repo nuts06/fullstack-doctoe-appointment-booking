ff05f3645ee9725bd9d86d4dbeba50dfc785f41b
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-07 12:45:19 +0530

    added the global css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home'
 import Login from './pages/Login';
+import About from './pages/About';
+import Contact from './pages/Contact'
 import Doctors from './pages/Doctors';
+import MyProfile from './pages/MyProfile';
+import MyAppointment from './pages/MyAppointment';
+import Appointment from './pages/Appointment';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <Navbar /> 
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path='/doctors' element={<Doctors />}/>
@@ -14,6 +21,9 @@ const App = () => {
         <Route path='/login' element={<Login />}/>
         <Route path='/about' element={<About />}/>
         <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />}/>
+        <Route path='/my-appointments' element={<MyAppointment />}/>
+        <Route path='/appointment/:docId' element={<Appointment />}/>
       </Routes>
     </div>
   )

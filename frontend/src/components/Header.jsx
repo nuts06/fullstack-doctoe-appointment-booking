d11dc6f60523c686ddd9d2101f1b116beafb1e39
Natasha <121818639+nuts06@users.noreply.github.com>
2025-04-07 18:32:29 +0530

    added the id inside the anchor tag

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
           <img className='w-24' src={assets.group_profiles} alt="group photo" />
           <p>Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block'/> schedule your appointment hassle-free</p>
         </div>
-        <a href="" className='flex items-center gap-2 bg-white rounded-full px-8 py-3 text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
+        <a href="#speciality" className='flex items-center gap-2 bg-white rounded-full px-8 py-3 text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
           Book Appointment <img className='w-3' src={assets.arrow_icon} alt="arrow-icon" />
         </a>
       </div>
